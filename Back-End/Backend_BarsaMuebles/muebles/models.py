79708accb79792f458dc4be1f4280b3f1b6996b1
HumbertoCreate <avalosgalvezjorge@gmail.com>
2025-04-01 12:38:12 -0600

    Correcciones de relaciones

--- Back-End/Backend_BarsaMuebles/muebles/models.py
+++ Back-End/Backend_BarsaMuebles/muebles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from categorias.models import Categorias
-
+from contenido.models import Contenido
 
 class Muebles(models.Model):
     categoryID = models.ForeignKey(Categorias, on_delete=models.PROTECT, related_name='categorias')
@@ -9,3 +9,4 @@ class Muebles(models.Model):
     descriptionFurniture = models.CharField(max_length=225)
     priceFurniture = models.DecimalField(max_digits=100, decimal_places=2, default=0)
     stateFurniture = models.BooleanField(default=True)
+    contenidoID = models.ManyToManyField(Contenido)
--- Back-End/Backend_BarsaMuebles/ventas/models.py
+++ Back-End/Backend_BarsaMuebles/ventas/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.db.models import PROTECT
 
 from muebles.models import Muebles
 
-
 class Ventas(models.Model):
-    furnitureID = models.ForeignKey(Muebles, on_delete=PROTECT, related_name='ventas')
+    furnitureID = models.ForeignKey(Muebles, on_delete=models.PROTECT, related_name='ventas')
     dateSale = models.DateField()
     amountSale = models.DecimalField(max_digits=100, decimal_places=2, default=0)
 
