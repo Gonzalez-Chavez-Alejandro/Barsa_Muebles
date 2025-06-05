e7b01c36adc851b7a6f17b055c426617c3d0b773
HumbertoCreate <avalosgalvezjorge@gmail.com>
2025-06-05 12:59:55 -0600

    .

---
+++ AplicacionMuebleraSahuayense/autentication/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'ageUser', 'phoneUser', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            ageUser=validated_data['ageUser'],
+            phoneUser=validated_data['phoneUser'],
+            password=validated_data['password']
+        )
+
+        return user
