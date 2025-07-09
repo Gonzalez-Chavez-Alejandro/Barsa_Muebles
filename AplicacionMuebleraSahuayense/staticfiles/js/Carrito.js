fe8cc87f0afd7fc858cb38b0cc2894ea4df0a4bb
Wolf <a17jul@gmail.com>
2025-07-09 07:54:04 -0600

    .

--- AplicacionMuebleraSahuayense/staticfiles/js/Carrito.js
+++ AplicacionMuebleraSahuayense/staticfiles/js/Carrito.js
@@ -369,7 +369,21 @@ document.addEventListener("DOMContentLoaded", () => {
     await procesarEncargo();
   });
 
-  document.getElementById("agregar-carrito-detalle")?.addEventListener("click", () => {
+  
+  validarTokenYUsuario();
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hash = window.location.hash;
+
+  if (hash === "#mi-perfil") {
+    if (typeof mostrarSeccion === "function") {
+      mostrarSeccion("mi-perfil");
+    }
+  }
+});
+document.getElementById("agregar-carrito-detalle")?.addEventListener("click", () => {
     if (!window.producto?.id) {
       mostrarToast("Producto no cargado aún.", "warning");
       return;
@@ -445,16 +459,3 @@ document.addEventListener("DOMContentLoaded", () => {
       mostrarToast("Ocurrió un error al agregar el producto.", "error");
     }
   }
-  validarTokenYUsuario();
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  const hash = window.location.hash;
-
-  if (hash === "#mi-perfil") {
-    if (typeof mostrarSeccion === "function") {
-      mostrarSeccion("mi-perfil");
-    }
-  }
-});
